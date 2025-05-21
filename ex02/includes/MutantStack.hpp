61b9dce434c23ca9d3563bbacec73fff7088363e
Tebourbi Gourgeau Sofien Victor <tsofien-@z2r12p6.42lyon.fr>
2025-05-21 23:15:06 +0200

    cpp 08 done

--- ex02/includes/MutantStack.hpp
+++ ex02/includes/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: tsofien- <tsofien-@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 15:54:24 by tsofien-          #+#    #+#             */
-/*   Updated: 2025/04/23 15:25:55 by tsofien-         ###   ########.fr       */
+/*   Updated: 2025/05/21 23:08:44 by tsofien-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,52 @@
 #include <iostream>
 #include <stack>
 #include <string>
+#include <deque>
 
-class MutantStack : public std::stack<int>
+template <typename T>
+class MutantStack : public std::stack<T>
 {
 public:
-	MutantStack(/* args */);
-	MutantStack(const MutantStack &src);
-	MutantStack &operator=(const MutantStack &rhs);
-	int &operator[](unsigned int index);
-	~MutantStack();
+	MutantStack(/* args */) {}
 
-	// getters & setters
-	std::stack<int> getStack() const;
-	std::deque<int> getContainerC() const;
+	MutantStack(const MutantStack &src)
+	{
+		*this = src;
+	}
+
+	MutantStack &operator=(const MutantStack &rhs)
+	{
+		if (this != &rhs)
+		{
+			this->c = rhs.c;
+		}
+		return *this;
+	}
+
+	// iterator
+	typedef typename std::deque<T>::iterator iterator;
+	iterator begin()
+	{
+		return this->c.begin();
+	}
+
+	iterator end()
+	{
+		return this->c.end();
+	}
+
+	typedef typename std::deque<T>::const_iterator const_iterator;
+	const_iterator begin() const
+	{
+		return this->c.begin();
+	}
+
+	const_iterator end() const
+	{
+		return this->c.end();
+	}
+
+	~MutantStack<T>(){};
 };
 
 #endif /***************************************************** MUTANTSTACK_HPP */
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tsofien- <tsofien-@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 09:58:51 by tsofien-          #+#    #+#             */
-/*   Updated: 2025/04/23 15:18:52 by tsofien-         ###   ########.fr       */
+/*   Updated: 2025/05/21 23:07:24 by tsofien-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,28 @@
 #include "MutantStack.hpp"
 #include <iterator>
 
-#define SIZE 5
-
-int main(void)
+int main()
 {
-
-	MutantStack stack;
-
-	std::cout << "Pushing 1, 2, 3, 4, 5 into the stack" << std::endl;
-	for (size_t i = 0; i < SIZE; i++)
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
 	{
-		stack.push(i + 1);
-		std::cout << "Pushed: " << i + 1 << std::endl;
+		std::cout << *it << std::endl;
+		++it;
 	}
-
-	stack.push(1);
-	if (!stack.getContainerC().size())
-		std::cout << "Container is empty" << std::endl;
-	else
-		std::cout << "Container is not empty: " << stack.getContainerC().size() << std::endl;
-	for (size_t i = 0; i < SIZE; i++)
-	{
-		stack.getContainerC().begin();
-	}
-
+	std::stack<int> s(mstack);
 	return 0;
 }
