f482050c67be641e4939bb477589adec93853642
Tebourbi Gourgeau Sofien Victor <tsofien-@z2r10p8.42lyon.fr>
2025-05-27 18:52:05 +0200

    update done + add few tests on exercice 2

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,36 +6,84 @@
 /*   By: tsofien- <tsofien-@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 09:58:51 by tsofien-          #+#    #+#             */
-/*   Updated: 2025/05/21 23:07:24 by tsofien-         ###   ########.fr       */
+/*   Updated: 2025/05/27 18:49:15 by tsofien-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.hpp"
 #include "MutantStack.hpp"
 #include <iterator>
+#include <algorithm>
+
+#define SIZE 10
+
+int generateRandomNumber(int max)
+{
+	if (max)
+		return rand() % max;
+	return rand();
+}
 
 int main()
 {
 	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
+
+	// int randomNumber = generateRandomNumber(100);
+
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		mstack.push(generateRandomNumber(100));
+	}
+
+	std::cout << GREEN << "Top element: " << mstack.top() << RESET << std::endl;
+	std::cout << BLUE << "Elements in the stack:" << RESET << std::endl;
+
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
 	++it;
 	--it;
 	while (it != ite)
 	{
-		std::cout << *it << std::endl;
+		std::cout << *it << " ";
 		++it;
 	}
-	std::stack<int> s(mstack);
+	std::cout << std::endl;
+
+	std::cout << MAGENTA << "Size of the stack: " << mstack.size() << RESET << std::endl;
+	mstack.pop();
+	std::cout << YELLOW << "After popping, size of the stack: " << mstack.size() << RESET << std::endl;
+
+	while (!mstack.empty())
+	{
+		mstack.pop();
+	}
+	std::cout << RED << "Stack cleared. Size of the stack: " << mstack.size() << RESET << std::endl;
+
 	return 0;
 }
+
+// int main()
+// {
+// 	MutantStack<int> mstack;
+// 	mstack.push(5);
+// 	mstack.push(17);
+// 	std::cout << mstack.top() << std::endl;
+// 	mstack.pop();
+// 	std::cout << mstack.size() << std::endl;
+// 	mstack.push(3);
+// 	mstack.push(5);
+// 	mstack.push(737);
+// 	//[...]
+// 	mstack.push(0);
+// 	MutantStack<int>::iterator it = mstack.begin();
+// 	MutantStack<int>::iterator ite = mstack.end();
+// 	++it;
+// 	--it;
+// 	while (it != ite)
+// 	{
+// 		std::cout << *it << std::endl;
+// 		++it;
+// 	}
+// 	std::stack<int> s(mstack);
+// 	return 0;
+// }
